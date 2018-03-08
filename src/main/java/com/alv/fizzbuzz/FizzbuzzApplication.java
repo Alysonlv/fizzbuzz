82d8da1a2ddbc33395a1c5ef72bd125aa8594f1a
Alyson Lima Vasconcelos <alysonlv@gmail.com>
2018-03-07 21:23:06 -0300

    FIX to turn the application able to deploy on external Tomcat and cloud

--- src/main/java/com/alv/fizzbuzz/FizzbuzzApplication.java
+++ src/main/java/com/alv/fizzbuzz/FizzbuzzApplication.java
@@ -1,11 +1,15 @@
 package com.alv.fizzbuzz;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class FizzbuzzApplication extends SpringBootServletInitializer {
 
 	@Override
